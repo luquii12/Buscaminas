319d7797ca73b0c44f103322ea78dcb63de21b88
luquii12 <lucassanchezsolera@gmail.com>
2024-02-27 10:47:53 +0100

    Crear las clases de la práctica
    Casilla: está acabada
    
    JuegoBuscaMinas: constructor hecho y tengo que terminar iniciarTablero
    
    UsoJuegoBuscaMinas: he creado un objeto de tipo JuegoBuscaMinas para probar iniciarTablero

--- src/Buscaminas/Casilla.java
+++ src/Buscaminas/Casilla.java
@@ -1,5 +1,45 @@
 package Buscaminas;
 
 public class Casilla {
+	private boolean tieneMina, estaMarcada, estaOculta;
+	private int numMinasCercanas;
 
+	public Casilla() {
+		tieneMina = false;
+		estaMarcada = false;
+		estaOculta = true;
+		numMinasCercanas = 0;
+	}
+
+	public boolean isTieneMina() {
+		return tieneMina;
+	}
+
+	public void setTieneMina(boolean tieneMina) {
+		this.tieneMina = tieneMina;
+	}
+
+	public boolean isEstaMarcada() {
+		return estaMarcada;
+	}
+
+	public void setEstaMarcada(boolean estaMarcada) {
+		this.estaMarcada = estaMarcada;
+	}
+
+	public boolean isEstaOculta() {
+		return estaOculta;
+	}
+
+	public void setEstaOculta(boolean estaOculta) {
+		this.estaOculta = estaOculta;
+	}
+
+	public int getNumMinasCercanas() {
+		return numMinasCercanas;
+	}
+
+	public void setNumMinasCercanas(int numMinasCercanas) {
+		this.numMinasCercanas = numMinasCercanas;
+	}
 }
