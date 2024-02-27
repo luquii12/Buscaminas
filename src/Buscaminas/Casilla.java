2ab2f737f5c72706ac31ae82a36186ee2885f5fe
luquii12 <lucassanchezsolera@gmail.com>
2024-02-27 09:14:27 +0100

    crear proyecto Buscaminas

---
+++ src/Buscaminas/Casilla.java
@@ -0,0 +1,5 @@
+package Buscaminas;
+
+public class Casilla {
+
+}
---
+++ src/Buscaminas/UsoJuegoBuscaMinas.java
@@ -0,0 +1,10 @@
+package Buscaminas;
+
+public class UsoJuegoBuscaMinas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
